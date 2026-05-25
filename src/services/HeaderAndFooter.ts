381b224515932ea5363f243f127981ba0c5d42ef
Hertchel <ahertchel@gmail.com>
2026-05-26 02:36:28 +0800

    aoq form improvement

--- src/services/HeaderAndFooter.ts
+++ src/services/HeaderAndFooter.ts
@@ -7,10 +7,11 @@ export const HeaderAndFooter = async (
   page: PDFPage,
   boldFont: PDFFont,
   timesRomanFont: PDFFont,
-  footerYPosition: number,
   timesBoldFont: PDFFont,
+  timesBoldItalicFont: PDFFont,
   bac_members: BACmemberType[],
-  data: supplierItemType_
+  data: supplierItemType_,
+  totalPages: number
 ) => {
     // Draw header
  
@@ -18,52 +19,66 @@ export const HeaderAndFooter = async (
     page.drawText('Project Name:', {x: 194.49, y: 473.19, size: 11, font: timesRomanFont });
     page.drawText('Date of Posting:', {x: 186.61, y: 456.38, size: 11, font: timesRomanFont });
     page.drawText('Project Location:', {x: 182, y: 442.91, size: 11, font: timesRomanFont });
+    page.drawText('Lamacan, Argao, Cebu', {x: 265, y: 442.91, size: 11, font: timesRomanFont });
     page.drawText('Implementing Office:', {x: 162.50, y: 428, size: 11, font: timesRomanFont });
+    page.drawText('Cebu Technological University - Argao Campus', {x: 265, y: 428, size: 11, font: timesRomanFont });
     page.drawText('Approved Budget:', {x: 176.70, y: 413, size: 11, font: timesRomanFont });
     page.drawText('# of Sheets:', {x: 644.29, y: 473.19, size: 11, font: timesRomanFont });
+    page.drawText(totalPages.toString(), { x: 700, y: 473.19, size: 11, font: timesRomanFont, });
     page.drawText('Award Resolution No.:', {x: 594, y: 456.38, size: 11, font: timesRomanFont });
     page.drawText('Date and Time:', {x: 627.70, y: 442.91, size: 11, font: timesRomanFont });
+    const currentDateTime = new Date().toLocaleString();
+    page.drawText(currentDateTime, { x: 700, y: 442.91, size: 11, font: timesRomanFont,});
     page.drawText('Mode of Procurement:', {x: 597, y: 428, size: 11, font: timesRomanFont });
     page.drawText('PR/Control No.:', {x: 624, y: 413, size: 11, font: timesRomanFont });
+
+    const prControlNo =data?.supplier_details?.aoq_details?.pr_details?.pr_no || "N/A";
+        page.drawText(prControlNo, { x: 700, y: 413, size: 11, font: timesRomanFont, });
     // Draw footer text near footerYPosition
    
 
     // Example signature fields at calculated positions
-    page.drawText('WE CERTIFY that we opened the bids of the above-listed materials, the abstract of which appears, as the time and date indicated.', {x: 56.69, y: footerYPosition, size: 12, font: timesRomanFont });
-    page.drawText('Bids and Awards Committee:', {x: 31.18, y: footerYPosition - 15, size: 12, font: timesRomanFont });
-    page.drawText('Conforme:', {x: 547.08, y: footerYPosition - 80, size: 11, font: timesRomanFont });
     page.drawText('No.', {x: 40, y: 380, size: 11, font: timesBoldFont });
-    page.drawText('Items', {x: 200, y: 380, size: 11, font: timesBoldFont });
-    page.drawText('Quantity', {x: 365, y: 380, size: 11, font: timesBoldFont });
+    page.drawText('Items', {x: 140, y: 380, size: 11, font: timesBoldFont });
+    page.drawText('Quantity', {x: 300, y: 380, size: 11, font: timesBoldFont });
   
-    page.drawText('Agency', {x: 428.07, y: 387, size: 11, font: timesBoldFont });
-    page.drawText('Price', {x: 433, y: 375, size: 11, font: timesBoldFont });
+    page.drawText('Agency', {x: 360, y: 387, size: 11, font: timesBoldFont });
+    page.drawText('Price', {x: 365, y: 375, size: 11, font: timesBoldFont });
   
-    page.drawText('WINNING BIDDER', {x: 578, y: 380, size: 11, font: timesBoldFont });
-    page.drawText('WINNING PRICE  ', {x: 795, y: 380, size: 11, font: timesBoldFont });
+    page.drawText('BIDDER 1', {x: 435, y: 390, size: 11, font: timesBoldFont });
+    page.drawText('BIDDER 2', {x: 525, y: 390, size: 11, font: timesBoldFont });
+    page.drawText('BIDDER 3', {x: 630, y: 390, size: 11, font: timesBoldFont });
+    page.drawText('WINNING BIDDER  ', {x: 750, y: 380, size: 11, font: timesBoldFont });
+    page.drawText('Lowest Complying Supplier ', {x: 285, y: 275, size: 11, font: timesBoldItalicFont });
   
   
     //text signature - FIXED with optional chaining and fallback values
-    page.drawText(bac_members[0]?.name || "N/A", {x: 80.39, y: footerYPosition - 57.15, size: 11, font: timesBoldFont });
-    page.drawText(bac_members[0]?.designation || "N/A", {x: 96.92, y: footerYPosition - 72.32, size: 11, font: timesRomanFont });
-
-    page.drawText(bac_members[1]?.name || "N/A", {x: 370, y: footerYPosition - 57.15, size: 11, font: timesBoldFont });
-    page.drawText(bac_members[1]?.designation || "N/A", {x: 381.93, y: footerYPosition - 72.32, size: 11, font: timesRomanFont });
+    page.drawText('WE CERTIFY that we opened the bids of the above-listed materials, the abstract of which appears, as the time and date indicated.', {x: 56.69, y: 230, size: 12, font: timesRomanFont });
+    page.drawText('Bids and Awards Committee:', {x: 31.18, y: 200, size: 12, font: timesRomanFont });
+    //bac member 1
+    page.drawText(bac_members[0]?.name || "N/A", {x: 80.39, y: 165, size: 11, font: timesBoldFont });
+    page.drawText(bac_members[0]?.designation || "N/A", {x: 96.92, y: 150, size: 11, font: timesRomanFont });
+    //bac member 2(to the left of bac member 1)
+    page.drawText(bac_members[1]?.name || "N/A", {x: 370, y: 165, size: 11, font: timesBoldFont });
+    page.drawText(bac_members[1]?.designation || "N/A", {x: 381.93, y: 150, size: 11, font: timesRomanFont });
+    //end-user text
+    page.drawText('End-user', {x: 726.77, y: 120 , size: 11, font: timesRomanFont });
+    //conforme text
+    page.drawText('Conforme:', {x: 547.08, y: 165, size: 11, font: timesRomanFont });
+    //bac member 3(below bac member 1)
+    page.drawText(bac_members[2]?.name || "N/A", {x: 68.57, y: 115, size: 11, font: timesBoldFont });
+    page.drawText(bac_members[2]?.designation || "N/A", {x: 96.92, y: 100, size: 11, font: timesRomanFont });
+    //bac vice chairman
+    page.drawText(bac_members[3]?.name || "N/A", {x: 375, y: 115, size: 11, font: timesBoldFont });
+    page.drawText(bac_members[3]?.designation || "N/A", {x: 371.14, y: 100  , size: 11, font: timesRomanFont });
+    //bac chairman
+    page.drawText(bac_members[4]?.name || "N/A", {x: 200, y: 65, size: 11, font: timesBoldFont });
+    page.drawText(bac_members[4]?.designation || "N/A", {x: 225.14, y: 50, size: 11, font: timesRomanFont });
   
-    page.drawText('End-user', {x: 726.77, y: footerYPosition - 130 , size: 11, font: timesRomanFont });
-  
-    page.drawText(bac_members[2]?.name || "N/A", {x: 68.57, y: footerYPosition - 130.07, size: 11, font: timesBoldFont });
-    page.drawText(bac_members[2]?.designation || "N/A", {x: 96.92, y: footerYPosition - 145.2 , size: 11, font: timesRomanFont });
-
-    page.drawText(bac_members[3]?.name || "N/A", {x: 375, y: footerYPosition - 130.07, size: 11, font: timesBoldFont });
-    page.drawText(bac_members[3]?.designation || "N/A", {x: 371.14, y: footerYPosition - 140.2  , size: 11, font: timesRomanFont });
-  
-    page.drawText(bac_members[4]?.name || "N/A", {x: 200, y:  footerYPosition - 215, size: 11, font: timesBoldFont });
-    page.drawText(bac_members[4]?.designation || "N/A", {x: 225.14, y:  footerYPosition - 230  , size: 11, font: timesRomanFont });
-  
-    // FIXED: Add optional chaining for nested data
-    page.drawText(data?.supplier_details?.aoq_details?.pr_details?.campus_director_details?.name || "N/A", {x: 690, y:  footerYPosition - 215, size: 11, font: timesBoldFont });
-    page.drawText(data?.supplier_details?.aoq_details?.pr_details?.campus_director_details?.designation || "N/A", {x: 665, y:  footerYPosition - 230, size: 11, font: timesRomanFont });
+    // FIXED: Add optional chaining for nested data 
+    // campus director details
+    page.drawText(data?.supplier_details?.aoq_details?.pr_details?.campus_director_details?.name || "N/A", {x: 690, y:  65, size: 11, font: timesBoldFont });
+    page.drawText(data?.supplier_details?.aoq_details?.pr_details?.campus_director_details?.designation || "N/A", {x: 730, y:  50, size: 11, font: timesRomanFont });
     
     //Horizontal Line
     page.drawLine({start: { x: 29.53  , y: 401.57 }, end: { x: 898.98, y: 401.57 }, thickness: 1.5 , color: rgb(0, 0, 0)});
@@ -102,13 +117,80 @@ export const HeaderAndFooter = async (
         width: jpgDims.width,
         height: jpgDims.height,
     });
+
+    /*
+    //  DEBUG GRID 
+    const GRID_SIZE = 15;
+    const PAGE_WIDTH = 936;
+    const PAGE_HEIGHT = 612;
+
+    // Vertical grid lines
+    for (let x = 0; x <= PAGE_WIDTH; x += GRID_SIZE) {
+
+    // Grid line
+    page.drawLine({
+        start: { x, y: 0 },
+        end: { x, y: PAGE_HEIGHT },
+        thickness: 0.3,
+        color: rgb(0.85, 0.85, 0.85),
+    });
+
+    // X-axis label at BOTTOM
+    page.drawText(`${x}`, {
+        x: x + 1,
+        y: 2,
+        size: 5,
+        font: timesRomanFont,
+        color: rgb(1, 0, 0),
+    });
+    }
+
+    // Horizontal grid lines
+    for (let y = 0; y <= PAGE_HEIGHT; y += GRID_SIZE) {
+
+    // Grid line
+    page.drawLine({
+        start: { x: 0, y },
+        end: { x: PAGE_WIDTH, y },
+        thickness: 0.3,
+        color: rgb(0.85, 0.85, 0.85),
+    });
+
+    // Y-axis label
+    page.drawText(`${y}`, {
+        x: 2,
+        y: y + 1,
+        size: 5,
+        font: timesRomanFont,
+        color: rgb(0, 0, 1),
+    });
+    }
+    */
     
     //Vertical Line
-    page.drawLine({start: { x: 29.53 , y:401.57  }, end: { x: 29.53 , y:  368.57 }, thickness: 1.5 , color: rgb(0, 0, 0)});
-    page.drawLine({start: { x: 64.96 , y:401.57  }, end: { x: 64.96 , y:  368.57 }, thickness: 1.5 , color: rgb(0, 0, 0)});
-    page.drawLine({start: { x: 356.14 , y:401.57 }, end: { x: 356.14 , y:  368.57  }, thickness: 1.5 , color: rgb(0, 0, 0)});
-    page.drawLine({start: { x: 415.20 , y:401.57  }, end: { x: 415.20 , y: 368.57 }, thickness: 1.5 , color: rgb(0, 0, 0)});
-    page.drawLine({start: { x: 480.31 , y:401.57  }, end: { x: 480.31 , y:  368.57 }, thickness: 1.5 , color: rgb(0, 0, 0)});
-    page.drawLine({start: { x: 780.38 , y:401.57  }, end: { x: 780.38 , y:  368.57}, thickness: 1.5 , color: rgb(0, 0, 0)});
-    page.drawLine({start: { x: 898.50, y:401.57  }, end: { x: 898.50 , y:  368.57  }, thickness: 1.5 , color: rgb(0, 0, 0)});
+    // 1
+    page.drawLine({start: { x: 29.53 , y:401.57  }, end: { x: 29.53 , y:  270 }, thickness: 1.5 , color: rgb(0, 0, 0)});
+    // 2
+    page.drawLine({start: { x: 64.96 , y:401.57  }, end: { x: 64.96 , y:  293 }, thickness: 1.5 , color: rgb(0, 0, 0)});
+    // 3
+    page.drawLine({start: { x: 290 , y:401.57  }, end: { x: 290 , y:  293 }, thickness: 1.5 , color: rgb(0, 0, 0)});
+    // 4
+    page.drawLine({start: { x: 320 , y:370 }, end: { x: 320 , y:  293 }, thickness: 1.5 , color: rgb(0, 0, 0)});
+    // 5
+    page.drawLine({start: { x: 350 , y:401.57 }, end: { x: 350 , y:  293 }, thickness: 1.5 , color: rgb(0, 0, 0)});
+    // 6
+    page.drawLine({start: { x: 415.20 , y:401.57  }, end: { x: 415.20 , y:  270 }, thickness: 1.5 , color: rgb(0, 0, 0)});
+    // 7
+    page.drawLine({start: { x: 515 , y:401.57  }, end: { x: 515 , y:  270 }, thickness: 1.5 , color: rgb(0, 0, 0)});
+    // 8
+    page.drawLine({start: { x: 605 , y:401.57  }, end: { x: 605 , y:  270 }, thickness: 1.5 , color: rgb(0, 0, 0)});
+    // 9
+    page.drawLine({start: { x: 705 , y:401.57  }, end: { x: 705 , y:  270}, thickness: 1.5 , color: rgb(0, 0, 0)});
+    // 10
+    page.drawLine({start: { x: 898.50, y:401.57  }, end: { x: 898.50 , y:  270  }, thickness: 1.5 , color: rgb(0, 0, 0)});
+
+    //horizontal line below the last row of items
+    //Below the "Lowest Complying Supplier" text
+    page.drawLine({ start: { x: 29.53, y: 270 }, end: { x: 898.98, y: 270 }, thickness: 1.5, color: rgb(0, 0, 0), });
+    page.drawLine({ start: { x: 415, y: 386 }, end: { x: 705, y: 386 }, thickness: 1.5, color: rgb(0, 0, 0), });   
 };
--- src/services/generateAOQPDF.ts
+++ src/services/generateAOQPDF.ts
@@ -2,7 +2,7 @@ import { PDFDocument, StandardFonts, rgb } from "pdf-lib";
 import { HeaderAndFooter } from "./HeaderAndFooter";
 import { supplierItemType_ } from "@/types/response/abstract-of-quotation";
 import { BACmemberType } from "@/types/request/BACmember";
-/*
+
 const drawDebugGrid = (page: any) => {
   const { width, height } = page.getSize();
 
@@ -53,7 +53,7 @@ const drawDebugGrid = (page: any) => {
     });
   }
 };
-*/
+
 export const generateAOQPDF = async (data: supplierItemType_[], bac_members: BACmemberType[]) => {
   const items = Array.isArray(data) ? data : [];
 
@@ -64,109 +64,86 @@ export const generateAOQPDF = async (data: supplierItemType_[], bac_members: BAC
 
   const pdfDoc = await PDFDocument.create();
   const timesBoldFont = await pdfDoc.embedFont(StandardFonts.TimesRomanBold);
+  const timesBoldItalicFont = await pdfDoc.embedFont( StandardFonts.TimesRomanBoldItalic );
   const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman);
-  const itemsPerPage = 10;
+  const itemsPerPage = 5;
   const pages = Math.ceil(items.length / itemsPerPage);
+
 
   for (let pageIndex = 0; pageIndex < pages; pageIndex++) {
     const page = pdfDoc.addPage([936, 612]);
-    // drawDebugGrid(page);
-    let yPosition = 355;
-    let yPosition1 = 353;
+    drawDebugGrid(page);
+    const ROW_HEIGHT = 15;
+    const FIRST_ROW_Y = 357;
     const pageItems = items.slice(
       pageIndex * itemsPerPage,
       (pageIndex + 1) * itemsPerPage
     );
     pageItems.forEach((item, index) => {
+      const rowY = FIRST_ROW_Y - (index * ROW_HEIGHT);
       const numtext = (index + 1).toString() || "";
       const numwidth = timesRomanFont.widthOfTextAtSize(numtext, 11);
       const numplace = (4 + 91.5) / 2;
       page.drawText(numtext, {
         x: numplace - numwidth / 2,
-        y: yPosition1,
+        y: rowY,
         size: 12,
         font: timesRomanFont,
       });
       const itemtext =
         item.item_quotation_details.item_details.item_description || "";
-      const itemwidth = timesRomanFont.widthOfTextAtSize(itemtext, 12);
-      const itemplace = (240 + 183.07) / 2;
       page.drawText(itemtext, {
-        x: itemplace - itemwidth / 2,
-        y: yPosition1,
+        x: 75,
+        y: rowY,
         size: 12,
         font: timesRomanFont,
       });
       const quantityText =
         item.item_quantity?.toString() || "";
-      const quantityWidth = timesRomanFont.widthOfTextAtSize(quantityText, 12);
-      const quantityColumnCenter = (405 + 366.14) / 2;
       page.drawText(quantityText, {
-        x: quantityColumnCenter - quantityWidth / 2,
-        y: yPosition1,
+        x: 295,
+        y: rowY,
         size: 11,
         font: timesRomanFont,
       });
       const agencypricePriceText =
         item.item_cost.toString() || "";
-      const agencypricepricewidth = timesRomanFont.widthOfTextAtSize(
-        agencypricePriceText,
-        12
-      );
       page.drawText(agencypricePriceText, {
-        x: 475 - agencypricepricewidth,
-        y: yPosition1,
+        x: 355,
+        y: rowY,
         size: 12,
         font: timesRomanFont,
       });
       const winningbiddertext = item.rfq_details.supplier_name || "";
-      const winningbidderwidth = timesRomanFont.widthOfTextAtSize(
-        winningbiddertext,
-        11
-      );
-      const winningbidderplace = (580 + 663.31) / 2;
       page.drawText(winningbiddertext, {
-        x: winningbidderplace - winningbidderwidth / 2,
-        y: yPosition1,
+        x: 715,
+        y: rowY,
         size: 12,
         font: timesRomanFont,
       });
       const winningPriceText =
         item.item_quotation_details.unit_price?.toString() || "";
-      const winningpricewidth = timesRomanFont.widthOfTextAtSize(
-        winningPriceText,
-        12
-      );
-      const winningpriceplace = 890;
       page.drawText(winningPriceText, {
-        x: winningpriceplace - winningpricewidth,
-        y: yPosition1,
+        x: 425,
+        y: rowY,
         size: 12,
         font: timesRomanFont,
       });
-
-      page.drawLine({
-        start: { x: 29.53, y: yPosition - 5 },
-        end: { x: 898.98, y: yPosition - 5 },
-        thickness: 1.5,
-        color: rgb(0, 0, 0),
-      });
-      yPosition -= 15;
-      yPosition1 -= 15;
     });
 
+    /*
     const verticalLinePositions = [
       29.53, 64.96, 356.14, 415.2, 480.31, 780.38, 898.5,
     ];
     verticalLinePositions.forEach((x) => {
       page.drawLine({
         start: { x, y: 368 },
-        end: { x, y: yPosition + 10 }, // Align to the last row's y-position
+        end: { x, y: 293 }, // Align to the last row's y-position
         thickness: 1.5,
         color: rgb(0, 0, 0),
       });
     });
-    const footerYPosition = yPosition - 10;
+    */
 
     console.log("BAC MEMBERS:", bac_members);
     console.log("BAC MEMBERS LENGTH:", bac_members?.length);
@@ -176,17 +153,36 @@ export const generateAOQPDF = async (data: supplierItemType_[], bac_members: BAC
       data[0]?.supplier_details?.aoq_details?.pr_details?.campus_director_details
     );
 
-    await HeaderAndFooter(
-      pdfDoc,
-      page,
-      timesBoldFont,
-      timesRomanFont,
-      footerYPosition,
-      timesBoldFont,
-      bac_members,
-      data[0]
-    );
+  const TABLE_TOP = 368;
+  const TOTAL_ROWS = 5;
+
+  for (let i = 0; i <= TOTAL_ROWS; i++) {
+
+    const lineY = TABLE_TOP - (i * ROW_HEIGHT);
+
+    page.drawLine({
+      start: { x: 29.53, y: lineY },
+      end: { x: 898.98, y: lineY },
+      thickness: 1.5,
+      color: rgb(0, 0, 0),
+    });
   }
+
+      await HeaderAndFooter(
+        pdfDoc,
+        page,
+        timesBoldFont,
+        timesRomanFont,
+       
+        timesBoldFont,
+        timesBoldItalicFont,
+        bac_members,
+        data[0],
+        pages
+      );
+    }
+
+  
 
   const pdfBytes = await pdfDoc.save();
   const fixedBuffer = new Uint8Array(pdfBytes).buffer;
